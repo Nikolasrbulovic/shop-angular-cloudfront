d97bc2cca4c45796da8204f94af1f8be0e91ac56
Nikola <n.srbulovic@hotmail.com>
2024-05-30 12:22:05 +0200

    feat(task6): async microservices communication

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -1,12 +1,14 @@
 const AWS = require("aws-sdk");
 const S3 = new AWS.S3();
 const csv = require("csv-parser");
+const SQS = new AWS.SQS();
 
 module.exports.handler = async (event) => {
   console.log("Event:", JSON.stringify(event, null, 2));
   const bucket = "my-import-bucket1";
   const key = "products.csv";
-
+  const queueUrl =
+    "https://sqs.us-east-1.amazonaws.com/471112551028/catalogItemsQueue";
   try {
     const params = {
       Bucket: bucket,
@@ -16,13 +18,21 @@ module.exports.handler = async (event) => {
     const s3Stream = S3.getObject(params).createReadStream();
 
     await new Promise((resolve, reject) => {
+      const records = [];
       s3Stream
         .pipe(csv())
-        .on("data", (data) => {
+        .on("data", async (data) => {
           console.log(`Record: ${JSON.stringify(data)}`);
+          records.push(data);
         })
-        .on("end", () => {
+        .on("end", async () => {
           console.log("CSV file successfully processed");
+          for (const record of records) {
+            await SQS.sendMessage({
+              QueueUrl: queueUrl,
+              MessageBody: JSON.stringify(record),
+            }).promise();
+          }
           resolve();
         })
         .on("error", (error) => {
